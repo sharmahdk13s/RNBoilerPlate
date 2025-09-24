5771f113a29068d2b719d09d77d689305844f68e
Hardik Sharma <hardik@mobmaxime.com>
2025-09-24 14:47:43 +0530

    Upgrade with latest react-native packages

--- template/android/app/src/main/java/com/boilerplatereactnative/MainApplication.kt
+++ template/android/app/src/main/java/com/boilerplatereactnative/MainApplication.kt
@@ -6,12 +6,10 @@ import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
+import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
-import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.react.modules.i18nmanager.I18nUtil;
-import com.facebook.soloader.SoLoader
 
 class MainApplication : Application(), ReactApplication {
 
@@ -36,13 +34,6 @@ class MainApplication : Application(), ReactApplication {
 
   override fun onCreate() {
     super.onCreate()
-    SoLoader.init(this, OpenSourceMergedSoMapping)
-    val sharedI18nUtilInstance = I18nUtil.getInstance()
-    sharedI18nUtilInstance.allowRTL(this, true)
-    // sharedI18nUtilInstance.forceRTL(this, false)
-    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      // If you opted-in for the New Architecture, we load the native entry point for this app.
-      load()
-    }
+    loadReactNative(this)
   }
 }
--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,12 +1,15 @@
 import store from '@redux/store';
 import checkLocalStorage from '@utils/handleLocalStorage';
 import React, { useLayoutEffect } from 'react';
+import { StatusBar, useColorScheme } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider } from 'react-redux';
 import Routes from './navigations/Routes';
 import './styles/unistyles';
 
 const App = (): React.JSX.Element => {
+  const isDarkMode = useColorScheme() === 'dark';
+
   useLayoutEffect(() => {
     checkLocalStorage();
   }, []);
@@ -14,6 +17,7 @@ const App = (): React.JSX.Element => {
   return (
     <Provider store={store}>
       <SafeAreaProvider>
+        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
         <Routes />
       </SafeAreaProvider>
     </Provider>
