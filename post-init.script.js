b52a0f823fdb12d429116f646572b2f267b8d23e
Hardik Sharma <hardik@mobmaxime.com>
2025-09-25 19:16:10 +0530

    Call rename project after all plugin setup finished

--- post-init.script.js
+++ post-init.script.js
@@ -1,7 +1,13 @@
 #!/usr/bin/env node
 const { rm } = require("fs").promises;
 const { applyPlugins } = require("./template/plugins");
+const { execSync } = require("child_process");
 
 applyPlugins().then(async () => {
-  await rm("./plugins", { recursive: true });
+  try {
+    await rm("./plugins", { recursive: true });
+    execSync("node scripts/rename.js", { stdio: "inherit" });
+  } catch (err) {
+    console.error("❌ Failed running applyPlugins:", err.message);
+  }
 });
