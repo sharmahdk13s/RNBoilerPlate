312060b349e813d56dd6b1b75c0d98bb11c87fc0
Hardik Sharma <hardik@mobmaxime.com>
2025-09-26 12:44:52 +0530

    Scripts changed update

--- post-init.script.js
+++ post-init.script.js
@@ -1,35 +1,11 @@
 #!/usr/bin/env node
 const { rm } = require("fs").promises;
 const { applyPlugins } = require("./template/plugins");
-const { execSync } = require("child_process");
 
-async function run() {
-  await applyPlugins();
-
+applyPlugins().then(async () => {
   try {
     await rm("./plugins", { recursive: true });
   } catch (err) {
     console.error("❌ Failed running applyPlugins:", err.message);
   }
-
-  // 🔄 Rename logic
-  const projectName = process.env.PROJECT_NAME || "MyApp";
-  const bundleId = process.env.BUNDLE_ID || `${projectName.toLowerCase()}`;
-
-  console.log(
-    `\n🔄 Renaming project to: ${projectName} (bundle: ${bundleId})\n`
-  );
-
-  try {
-    execSync("git init", { stdio: "inherit" });
-    execSync(`npx react-native-rename "${projectName}" -b ${bundleId}`, {
-      stdio: "inherit",
-    });
-    console.log("✅ Rename completed!");
-  } catch (e) {
-    console.error("❌ Rename failed:", e.message);
-    process.exit(1);
-  }
-}
-
-run();
+});
